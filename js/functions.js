65f8d3999dc72be2a296bee655e4e6020cbd7618
Faved <faved101@gmail.com>
2013-01-28 18:04:37 +0000

    changed the layout, added some css rules

--- js/functions.js
+++ js/functions.js
@@ -3,4 +3,24 @@
 $(function() {
     $( "#tabs" ).tabs();
     $( "#tabs2" ).tabs();
+    setDriverHeight();
   });
+
+// setting the hieght of the driver_avail divs
+function setDriverHeight()
+{
+	var i = 0;
+	// find out how many there are in the drivers div.
+	$('#DriversAvail div').each(function(){
+		i++;
+	});
+
+	//get the overall hieght of the containing div
+	var h = $("#DriversAvail").height() -i;
+	h = h/i;
+	h= h-5;
+	$('#DriversAvail div').each(function(){
+		$(this).css('height',h);
+		$(this).css('color','red');
+	});
+}
