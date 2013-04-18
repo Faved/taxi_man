ab5b0ba0452d6253587eb8c5778e96f5e89ffe22
Faved <faved101@gmail.com>
2013-04-18 14:48:41 +0100

    Changed admin registers

--- tamas/admin.py
+++ tamas/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from tamas.models import *
+from django.contrib.sites.models import Site
 
+class BookingAdmin(admin.ModelAdmin):
+	list_filter = ['date']
+	search_fields = ['pickup_address']
 
-admin.site.register(Booking)
+admin.site.register(Booking,BookingAdmin)
 admin.site.register(Driver)
 admin.site.register(Account)
 admin.site.register(Rotas)
 admin.site.register(Driver_rotas)
+admin.site.register(Escort)
+admin.site.unregister(Site)
